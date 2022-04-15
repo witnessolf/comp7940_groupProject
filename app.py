6d4d5522607829aa647fd94025f5b0a65496bcfd
witnessolf <1713157566@qq.com>
2022-04-15 11:09:20 +0800

    Realize the functions

--- app.py
+++ app.py
@@ -1,35 +1,115 @@
-## chatbot.py
-import telegram
-from telegram.ext import Updater, MessageHandler, Filters
-# The messageHandler is used for all message updates
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import urllib
+from io import BytesIO
+
+
 import configparser
 import logging
 
+cred = credentials.Certificate('./serviceAccount.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://comp7940project-34c59-default-rtdb.firebaseio.com"
+})
+
 
 def main():
-    # Load your token and create an Updater for your Bot
     config = configparser.ConfigParser()
     config.read('config.ini')
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
+
     # You can set this logging module, so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
 
     # register a dispatcher to handle message: here we register an echo dispatcher
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), comment)
     dispatcher.add_handler(echo_handler)
+    dispatcher.add_handler(MessageHandler(Filters.photo, photo))
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("getComments", getComments))
+    dispatcher.add_handler(CommandHandler("getImgs", getImgs))
 
     # To start the bot:
     updater.start_polling()
     updater.idle()
 
 
-def echo(update, context):
-    reply_message = update.message.text.upper()
-    logging.info("Update: " + str(update))
-    logging.info("context: " + str(context))
-    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+def comment(update, context):
+    try:
+        context_list = update.message.text.split(":")
+        logging.info(context_list)
+        title = context_list[0]
+        review = context_list[1]
+        ref = db.reference("/TvShows")
+        ref.push().set({
+            "title": title,
+            "review": review
+        })
+        context.bot.send_message(chat_id=update.effective_chat.id, text='comment successfully')
+    except (IndexError, ValueError):
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Please add comments according to this format <title:comment>')
+
+
+def getComments(update: Update, context: CallbackContext) -> None:
+    try:
+        ref = db.reference("/TvShows")
+        comment_data = ref.get()
+        title = context.args[0]
+        flag = False
+        for key, value in comment_data.items():
+            if value["title"] == title:
+                flag = True
+        if flag is False:
+            update.message.reply_text("There is not review on\"" + title + "\" !")
+        else:
+            update.message.reply_text('Here are some reviews on \"' + title + "\" :")
+            for key, value in comment_data.items():
+                if value["title"] == title:
+                    update.message.reply_text(value["review"])
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /getComment <title>')
+
+
+def photo(update: Update, context: CallbackContext) -> None:
+    pic = update.message.photo[-1].get_file()
+    logging.info(pic)
+    logging.info(pic["file_path"])
+    ref = db.reference("/hiking_img")
+    ref.push().set({
+        "img_id": pic["file_unique_id"],
+        "img_path": pic["file_path"]
+    })
+    update.message.reply_text('You\'ve successfully shared an image!')
+
+
+def getImgs(update: Update, context: CallbackContext) -> None:
+    try:
+        ref = db.reference("/hiking_img")
+        num = int(context.args[0])
+        photo_data = ref.get()
+        bot = context.bot
+        index = 0
+        if photo_data is None:
+            update.message.reply_text('Sorry, no one has shared the image yet')
+        update.message.reply_text('Here are some photos shared by others')
+        for key, value in photo_data.items():
+            num -= 1
+            index += 1
+            img = BytesIO(urllib.request.urlopen(value["img_path"]).read())
+            bot.send_photo(update.message.chat_id, img)
+            if num <= 0:
+                break
+        if num > 0:
+            update.message.reply_text('Sorry, we only have ' + str(index) + ' pictures at the moment')
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /getImgs <number>')
 
 
 if __name__ == '__main__':
--- database_test.py
+++ database_test.py
@@ -10,7 +10,21 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': "https://comp7940project-34c59-default-rtdb.firebaseio.com"
 })
 
-ref = db.reference("/")
+ref = db.reference("/Books/item")
+data = ref.get()
+if data is None:
+    print("a is none")
+    ref = db.reference("/Books")
+    ref.set({
+        "review": -1
+    })
+ref = db.reference("/Books/review")
+ref.push().set("aaaaa")
+ref.push().set("bbbbb")
+
+
+
+
 # 写到数据库中
 # with open("book_info.json", "r") as f:
 # 	file_contents = json.load(f)
@@ -18,38 +32,38 @@ ref = db.reference("/")
 
 # Firebase 为我们提供了push()将数据保存在唯一系统生成的密钥下的功能。
 # 此方法确保如果对同一个键执行多个写入，它们不会覆盖自己。
-ref = db.reference("/")
-ref.set({
-    "Books":
-        {
-            "Best_Sellers": -1
-        }
-})
-
-ref = db.reference("/Books/Best_Sellers")
-
-with open("book_info.json", "r") as f:
-    file_contents = json.load(f)
-
-for key, value in file_contents.items():
-    ref.push().set(value)
-
-# 修改
-ref = db.reference("/Books/Best_Sellers/")
-best_sellers = ref.get()
-print(best_sellers)
-for key, value in best_sellers.items():
-    if value["Author"] == "J.R.R. Tolkien":
-        value["Price"] = 90
-        ref.child(key).update({"Price": 80})
-
-# 查询
-ref = db.reference("/Books/Best_Sellers/")
-print(ref.order_by_child("Price").get())
-# 该方法的返回值是一个 OrderedDict。要按键排序，请使用order_by_key()。
-ref.order_by_child("Price").limit_to_last(1).get()
-# 要获得准确定价为 80 单位的书籍
-ref.order_by_child("Price").equal_to(80).get()
+# ref = db.reference("/")
+# ref.set({
+#     "Books":
+#         {
+#             "Best_Sellers": -1
+#         }
+# })
+#
+# ref = db.reference("/Books/Best_Sellers")
+#
+# with open("book_info.json", "r") as f:
+#     file_contents = json.load(f)
+#
+# for key, value in file_contents.items():
+#     ref.push().set(value)
+#
+# # 修改
+# ref = db.reference("/Books/Best_Sellers/")
+# best_sellers = ref.get()
+# print(best_sellers)
+# for key, value in best_sellers.items():
+#     if value["Author"] == "J.R.R. Tolkien":
+#         value["Price"] = 90
+#         ref.child(key).update({"Price": 80})
+#
+# # 查询
+# ref = db.reference("/Books/Best_Sellers/")
+# print(ref.order_by_child("Price").get())
+# # 该方法的返回值是一个 OrderedDict。要按键排序，请使用order_by_key()。
+# ref.order_by_child("Price").limit_to_last(1).get()
+# # 要获得准确定价为 80 单位的书籍
+# ref.order_by_child("Price").equal_to(80).get()
 
 # 删
 # for key, value in best_sellers.items():
